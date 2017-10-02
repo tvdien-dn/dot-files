9b83360c35ddccbcf4940223657461ec53bfbe2c
tvdien-dn <tvdien.dn@gmail.com>
2017-10-03 00:07:00 +0900

    emacs: google translate

---
+++ .emacs.d/inits/90google-translate.el
@@ -0,0 +1,10 @@
+;; (require 'google-translate)
+;; (require 'google-translate-default-ui)
+;; (global-set-key "\C-ct" 'google-translate-at-point)
+;; (global-set-key "\C-cT" 'google-translate-query-translate)
+
+(require 'google-translate)
+(require 'google-translate-smooth-ui)
+(global-set-key "\C-ct" 'google-translate-smooth-translate)
+
+(provide '90google-translate)
