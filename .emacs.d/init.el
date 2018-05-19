038954f2b1fbfa72dae2afec931e1a5f154ddfc4
tvdien-dn <tvdien.dn@gmail.com>
2018-05-19 16:22:30 +0900

    ivy mode

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -27,7 +27,7 @@
  '(markdown-command "/usr/local/bin/markdown")
  '(package-selected-packages
    (quote
-    (ac-php company-php docker-compose-mode dockerfile-mode magit json-mode ac-js2 js2-mode js2-refactor adoc-mode format-sql plantuml-mode flymake-lua lua-mode fzf markdown-preview-mode google-translate ag jinja2-mode mmm-jinja2 highlight-indent-guides sql-indent framemove markdown-preview-eww flymd vue-mode egg hideshow-org anzu yaml-mode color-theme-modern undo-tree markdown-mode php-mode flycheck-color-mode-line flycheck editorconfig web-mode web-mode-edit-element smart-mode-line xelb el-init company)))
+    (counsel ac-php company-php docker-compose-mode dockerfile-mode magit json-mode ac-js2 js2-mode js2-refactor adoc-mode format-sql plantuml-mode flymake-lua lua-mode fzf markdown-preview-mode google-translate ag jinja2-mode mmm-jinja2 highlight-indent-guides sql-indent framemove markdown-preview-eww flymd vue-mode egg hideshow-org anzu yaml-mode color-theme-modern undo-tree markdown-mode php-mode flycheck-color-mode-line flycheck editorconfig web-mode web-mode-edit-element smart-mode-line xelb el-init company)))
  '(safe-local-variable-values
    (quote
     ((eval flycheck-disable-checker
