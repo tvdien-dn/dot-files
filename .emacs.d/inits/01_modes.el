6f658b6ed3feb9e8e03b1c70303593393854a414
tvdien <tvdien.dn@gmail.com>
2019-05-12 14:26:17 +0900

    update emacs settings

--- .emacs.d/inits/01_modes.el
+++ .emacs.d/inits/01_modes.el
@@ -135,4 +135,13 @@
 ;; Gherkin/Cucumber major mode
 (use-package feature-mode
   :mode "\\.feature\\'")
+
+(eval-after-load "hideshow"
+  '(add-to-list 'hs-special-modes-alist
+    `(ruby-mode
+      ,(rx (or "def" "class" "module" "do" "{" "[")) ; Block start
+      ,(rx (or "}" "]" "end"))                       ; Block end
+      ,(rx (or "#" "=begin"))                        ; Comment start
+      ruby-forward-sexp nil)))
+
 ;;; 01_modes ends here
---
+++ .emacs.d/transient/history.el
@@ -0,0 +1,8 @@
+((magit-blame
+  ("-w"))
+ (magit-commit
+  ("--verbose"))
+ (magit-dispatch nil)
+ (magit-file-dispatch nil)
+ (magit-log
+  ("-n256" "--graph" "--decorate")))
