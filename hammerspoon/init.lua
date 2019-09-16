95dc88a31236dd036d448955913c2eab7f3a786b
tvdien <tvdien.dn@gmail.com>
2019-09-16 13:19:29 +0900

    remove shortcut confict with passord manager

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -14,5 +14,24 @@ spoon.ReloadConfiguration:start()
 
 hs.loadSpoon("WindowScreenLeftAndRight")
 hs.loadSpoon("WindowHalfsAndThirds")
-spoon.WindowHalfsAndThirds:bindHotkeys(spoon.WindowHalfsAndThirds.defaultHotkeys)
+spoon.WindowHalfsAndThirds:bindHotkeys({
+   left_half    = { {"ctrl",        "cmd"}, "Left" },
+   right_half   = { {"ctrl",        "cmd"}, "Right" },
+   top_half     = { {"ctrl",        "cmd"}, "Up" },
+   bottom_half  = { {"ctrl",        "cmd"}, "Down" },
+   third_left   = { {"ctrl", "alt"       }, "Left" },
+   third_right  = { {"ctrl", "alt"       }, "Right" },
+   third_up     = { {"ctrl", "alt"       }, "Up" },
+   third_down   = { {"ctrl", "alt"       }, "Down" },
+   top_left     = { {"ctrl",        "cmd"}, "1" },
+   top_right    = { {"ctrl",        "cmd"}, "2" },
+   bottom_left  = { {"ctrl",        "cmd"}, "3" },
+   bottom_right = { {"ctrl",        "cmd"}, "4" },
+   max_toggle   = { {"ctrl", "alt", "cmd"}, "f" },
+   max          = { {"ctrl", "alt", "cmd"}, "Up" },
+   -- undo         = { {        "alt", "cmd"}, "z" },
+   -- center       = { {        "alt", "cmd"}, "c" },
+   larger       = { {        "alt", "cmd", "shift"}, "Right" },
+   smaller      = { {        "alt", "cmd", "shift"}, "Left" },
+})
 spoon.WindowScreenLeftAndRight:bindHotkeys(spoon.WindowScreenLeftAndRight.defaultHotkeys)
